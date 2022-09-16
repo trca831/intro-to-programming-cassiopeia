e4e9dea2ec2a0d5d81615515dcfcb28109cbdee6
trca831 <tracycano3.14@gmail.com>
2022-09-15 18:05:37 -0700

    <message>

---
+++ instructions/js/index.js
@@ -0,0 +1,27 @@
+const today = new Date(); 
+
+let thisYear = today.getFullYear();
+
+// console.log(thisYear);
+
+let footer = document.querySelector('footer'); 
+
+let copyright = document.createElement('p');
+
+copyright.innerHTML = `Tracy ${thisYear}`;  
+
+footer.appendChild(copyright);
+
+//console.log(footer);
+
+let skills = ["JavaScript", "Java", "HTML5"];
+
+let skillsSection = document.querySelector('#skills');
+
+let skillsList = skillsSection.querySelector("ul");
+
+for(let i = 0; i < skills.length; i++){
+    let skill = document.createElement('li');
+    skill.innerText = skills[i];
+    skillsList.appendChild(skill);
+}
