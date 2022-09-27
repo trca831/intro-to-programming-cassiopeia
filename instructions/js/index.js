3e8191559486b97a7f2ccd98cf94b78430639f9f
trca831 <103814052+trca831@users.noreply.github.com>
2022-09-27 15:08:57 -0700

    wip2

--- instructions/js/index.js
+++ instructions/js/index.js
@@ -1,9 +1,5 @@
 const today = new Date(); 
-
 let thisYear = today.getFullYear();
-
-// console.log(thisYear);
-
 let footer = document.querySelector('footer'); 
 
 let copyright = document.createElement('p');
@@ -11,9 +7,7 @@ copyright.innerHTML = `© Tracy Cano ${thisYear}`;
 footer.appendChild(copyright);
 
 let skills = ["JavaScript", "Java", "HTML5"];
-
 let skillsSection = document.querySelector('#skills');
-
 let skillsList = skillsSection.querySelector("ul");
 
 for(let i = 0; i < skills.length; i++){
@@ -23,30 +17,46 @@ for(let i = 0; i < skills.length; i++){
 }
 
 let messageForm = document.querySelector('[name="leave_message"]');
-
+let messageCount = 0;
 messageForm.addEventListener("submit", (e) => { 
-     let name = e.target.name.value;
-     let email = e.target.email.value;
+    let name = e.target.name.value;
+    let email = e.target.email.value;
     let message = e.target.message.value;
   console.log("name: " + name);
   console.log("email: " + email);
   console.log("message: " + message);
 e.preventDefault();
 let messageSection = document.querySelector("#messages");
+let messageHeader = document.querySelector("#messages_header");
+messageHeader.innerHTML= "Messages";
 let messageList = messageSection.querySelector("ul");
 let newMessage = document.createElement("li");
 newMessage.innerHTML = `<a href="mailto:${email}">${name}</a> wrote: <span>${message}</span>  `;
 let removeButton = document.createElement("button");
 removeButton.innerText = "remove";
 removeButton.type = "button";
+let editButton = document.createElement("button");
+editButton.innerText = "edit";
+editButton.type = "button";
 removeButton.addEventListener("click", (e) => {
-    let entry = removeButton.parentNode();
+  messageCount--;
+  console.log(messageCount);
+    let entry = removeButton.parentNode;
     entry.remove();
-    
-})
+    if(messageCount == 0){
+      messageHeader.remove();
+        }
+});
 
 newMessage.appendChild(removeButton);
+newMessage.appendChild(editButton);
 messageList.appendChild(newMessage);
 messageForm.reset;
+messageCount++;
+    editButton.addEventListener("click", (e)=> {
+      e.target.name.value = name;
+      console.log(name);
+    });
+
 });
 
